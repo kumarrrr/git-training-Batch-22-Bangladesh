886c6550daa197e91e672f49e4322d411d99463f
Kumar,Nirmal <er.kumarnirmal@gmail.com>
2022-07-13 15:34:32 +0530

    info added

--- start.js
+++ start.js
@@ -1,2 +1,3 @@
 This is again starting the classes for the another batch,same will be shared with new changining params
 "the key to growth is regular progress"
+"there will be a upcoming reacr web application batch going to be started"
